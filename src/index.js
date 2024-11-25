fbbe10fab10638c834226960fde24b72144bd39e
hp <zekariastamiru12@gmail.com>
2024-11-26 00:52:42 +0300

    Creating the Stars

--- src/index.js
+++ src/index.js
@@ -8,7 +8,6 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <StarRating maxRating={10} />
-    <StarRating />
     {/* <App /> */}
   </React.StrictMode>
 );
