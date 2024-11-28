d1c4710f45e16f96b256675964ce85425d5aa6df
hp <zekariastamiru12@gmail.com>
2024-11-28 22:59:42 +0300

    fetch data

--- src/App v1.js
+++ src/App v1.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 const tempMovieData = [
   {
@@ -49,23 +49,16 @@ const tempWatchedData = [
 
 const average = (arr) =>
   arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
-const KEY = "6e0f586";
 
 export default function App() {
-  const [movies, setMovies] = useState([]);
-  const [watched, setWatched] = useState([]);
-
-  useEffect(function () {
-    fetch(`http://www.omdbapi.com/?apikey=${KEY}&s=interstellar`)
-      .then((res) => res.json())
-      .then((data) => setMovies(data.Search));
-    // .catch((err) => console.error(err));
-  }, []);
+  const [movies, setMovies] = useState(tempMovieData);
+  const [watched, setWatched] = useState(tempWatchedData);
 
   return (
     <>
       <NavBar>
         <Logo />
+        <Search />
         <NumResults movies={movies} />
       </NavBar>
 
@@ -160,7 +153,7 @@ function MovieList({ movies }) {
   return (
     <ul className="list">
       {movies?.map((movie) => (
-        <Movie movie={movie} key={movie.imdbID} />
+        <Movie movie={movies} />
       ))}
     </ul>
   );
--- src/Star.js
+++ src/Star.js
@@ -14,7 +14,9 @@ StarRating.propTypes = {
   size: PropTypes.number,
   messages: PropTypes.array,
   className: PropTypes.string,
+  onSetRating: PropTypes.func,
 };
+
 const starContainerStyle = {
   display: "flex",
 };
@@ -26,22 +28,28 @@ export default function StarRating({
   className = "",
   messages = [],
   defaultRating = 0,
-  onSetRating,
+  onSetRating = () => {},
 }) {
   const [rating, setRating] = useState(defaultRating);
   const [tempRating, setTempRating] = useState(0);
 
-  function HandleOnRating(rating) {
+  function handleOnRating(rating) {
     setRating(rating);
     onSetRating(rating);
   }
+
   const textStyle = {
     lineHeight: "1",
     margin: "0",
     fontSize: `${size / 1.5}px`,
     color: color,
-    className: { className },
   };
+
+  const displayMessage =
+    messages.length === maxRating
+      ? messages[tempRating ? tempRating - 1 : rating - 1] || ""
+      : tempRating || rating || "";
+
   return (
     <div style={containerStyle}>
       <div style={starContainerStyle}>
@@ -49,7 +57,7 @@ export default function StarRating({
           <Star
             key={i}
             full={tempRating ? tempRating >= i + 1 : rating >= i + 1}
-            onRate={() => HandleOnRating(i + 1)}
+            onRate={() => handleOnRating(i + 1)}
             onHoverIn={() => setTempRating(i + 1)}
             onHoverOut={() => setTempRating(0)}
             color={color}
@@ -57,11 +65,7 @@ export default function StarRating({
           />
         ))}
       </div>
-      <p style={textStyle}>
-        {messages.length === maxRating
-          ? messages[tempRating ? tempRating - 1 : rating - 1]
-          : tempRating || rating || ""}
-      </p>
+      <p style={textStyle}>{displayMessage}</p>
     </div>
   );
 }
@@ -97,11 +101,11 @@ function Star({ onRate, full, onHoverOut, onHoverIn, color, size }) {
           fill="none"
           viewBox="0 0 24 24"
           stroke={color}
+          strokeWidth="2"
         >
           <path
             strokeLinecap="round"
             strokeLinejoin="round"
-            strokeWidth="{2}"
             d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z"
           />
         </svg>
